93c477b1aa755d6e402cd6db450673d195b35f1c
Jefferson <jeffarrubla@gmail.com>
2023-03-09 18:31:42 -0500

    Move django key to env file

--- marvel/marvel/settings.py
+++ marvel/marvel/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*_if2@73anqakvrnbk%z97*ae-zo+=3o)*r2088x&7hq21(ez6,'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','Default'),
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
